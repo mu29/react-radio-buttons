7445f148abb7624f0228bb188d996ae0ac6bf2a0
InJung Chung <mu29@yeoubi.net>
2016-10-07 14:40:43 +0900

    Fix build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   module: {
     loaders: [
-      {test: /\.jsx?$/, exclude: /build|node_modules/, loader: 'babel-loader?stage=0'},
+      {test: /\.js?$/, exclude: /build|node_modules/, loader: 'babel-loader?stage=0'},
     ]
   },
   resolve: {
